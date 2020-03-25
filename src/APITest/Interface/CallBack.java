9ac10fe0fae03ea0871d7a265e34a1ed7d3b5844
quentin-lipeng <1667007705@qq.com>
2020-03-25 19:50:25 +0800

    delete

--- src/APITest/Interface/CallBack.java
+++
@@ -1,7 +0,0 @@
-package APITest.Interface;
-
-public class CallBack {
-    public static void main(String[] args) {
-
-    }
-}
--- src/APITest/Interface/SaySomeThing.java
+++
@@ -1,9 +0,0 @@
-package APITest.Interface;
-
-public interface SaySomeThing {
-    void sayName();
-    String name = "lee";
-    default String printName(){
-        return name;
-    }
-}
