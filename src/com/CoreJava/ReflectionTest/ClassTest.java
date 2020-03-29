86cb94f7e754cc802b049baddccbb4dd4119d4f9
quentin-lipeng <1667007705@qq.com>
2020-03-29 22:52:59 +0800

    获取成员变量

--- src/com/CoreJava/ReflectionTest/ClassTest.java
+++ src/com/CoreJava/ReflectionTest/ClassTest.java
@@ -4,33 +4,37 @@ import java.lang.reflect.Field;
 
 public class ClassTest {
     public static void main(String[] args) {
-        Class personClass = Person.class;
+        Class<Person> personClass = Person.class;
+        Person p = new Person();
 
         //获取public修饰的成员变量
         Field[] fields = personClass.getFields();
+        System.out.println("所有public成员变量--->");
         for (Field field : fields) System.out.println(field);
-        Person p = new Person();
-
-
 
         try {
+            //当前只能设置public成员变量
             Field field= personClass.getField("sex");
-            field.set(p,"qun");
-            Object name = field.get(p);
+            field.set(p,"male");
+            System.out.println("打印Person对象--->");
             System.out.println(p);
         } catch (NoSuchFieldException | IllegalAccessException e) {
             e.printStackTrace();
         }
 
-        //获取所有成员变量
+        //获取所有成员变量(包括非public)
         Field[] fields1 = personClass.getDeclaredFields();
+        System.out.println("所有成员变量--->");
         for (Field field : fields1) System.out.println(field);
 
         try {
             Field field2 = personClass.getDeclaredField("name");
             //忽略安全检查 就可以访问非public修饰的成员变量
             field2.setAccessible(true);
+            field2.set(p ,"qun" );
+            System.out.println("获取私有成员变量--->");
             System.out.println(field2.get(p));
+            System.out.println(p);
         } catch (NoSuchFieldException | IllegalAccessException e) {
             e.printStackTrace();
         }
--- src/com/CoreJava/ThreadTest/README.md
+++ src/com/CoreJava/ThreadTest/README.md
@@ -5,4 +5,6 @@
     1. 通过继承Thread类编写run方法 再通过Start实现创建线程开辟新栈
     2. 通过实现Runnable接口 实现run方法 把实现Runnable接口的类传到Thread对象构造器中
 ## Thread下的方法
-    1. sleep()
+```java
+1. sleep()
+```
