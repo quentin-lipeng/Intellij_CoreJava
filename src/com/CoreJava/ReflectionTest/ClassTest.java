1cb5619673e771f472c43d0348384e8bab176478
quentin-lipeng <1667007705@qq.com>
2020-03-29 22:41:49 +0800

    获取成员变量

--- src/com/CoreJava/ReflectionTest/ClassTest.java
+++ src/com/CoreJava/ReflectionTest/ClassTest.java
@@ -1,12 +1,38 @@
 package com.CoreJava.ReflectionTest;
 
-import java.util.Random;
+import java.lang.reflect.Field;
 
 public class ClassTest {
     public static void main(String[] args) {
-        Class cl1 = Random.class;
-        Class cl2 = int.class;
-        System.out.println(cl1);
-        System.out.println(cl2);
+        Class personClass = Person.class;
+
+        //获取public修饰的成员变量
+        Field[] fields = personClass.getFields();
+        for (Field field : fields) System.out.println(field);
+        Person p = new Person();
+
+
+
+        try {
+            Field field= personClass.getField("sex");
+            field.set(p,"qun");
+            Object name = field.get(p);
+            System.out.println(p);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        //获取所有成员变量
+        Field[] fields1 = personClass.getDeclaredFields();
+        for (Field field : fields1) System.out.println(field);
+
+        try {
+            Field field2 = personClass.getDeclaredField("name");
+            //忽略安全检查 就可以访问非public修饰的成员变量
+            field2.setAccessible(true);
+            System.out.println(field2.get(p));
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
